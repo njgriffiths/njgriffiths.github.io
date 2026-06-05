390d8343068fe0a1c66784a9fb1be805ea237c24
Nathan Griffiths <ngriffiths@postmedia.com>
2026-06-04 21:51:02 -0700

    Updates

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 const init = () => {
     new LazyLoad();
 
-    const lightbox = new FsLightbox();
-    const openLightbox = document.getElementById('image-container');
-    lightbox.props.sources = [
+    // print gfx
+    const imageLightbox = new FsLightbox();
+    const openImageLightbox = document.getElementById('image-container');
+    imageLightbox.props.sources = [
         './images/print-gfx/public-sector-salaries.jpg',
         './images/print-gfx/poli-tweets.jpg',
         './images/print-gfx/oly-anniversary.jpg',
@@ -12,8 +13,26 @@ const init = () => {
         './images/print-gfx/isp-complaints.jpg',
         './images/print-gfx/ballot-data.jpg'
     ];
-    openLightbox.addEventListener('click', () => {
-        lightbox.open();
+    imageLightbox.props.type = 'image';
+    openImageLightbox.addEventListener('click', () => {
+        imageLightbox.open();
+    });
+    
+    // illos
+    const illoLightbox = new FsLightbox();
+    const openIlloLightbox = document.getElementById('illo-container');
+    illoLightbox.props.sources = [
+        './images/illos/blue-kiyoji.jpg',
+        './images/print-gfx/public-sector-salaries.jpg',
+        './images/illos/rsf-one-pager.jpg',
+        './images/illos/cancelled-trip.jpg',
+        // './images/illos/covid-01.jpg',
+        './images/illos/mary-ann-shad.jpg',
+        './images/illos/solitary.jpg'
+    ];
+    illoLightbox.props.type = 'image';
+    openIlloLightbox.addEventListener('click', () => {
+        illoLightbox.open();
     });
 };
 
